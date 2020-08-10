59108b7494f3c30956956f84511362da43166cff
dongnv2804 <dongnv2804@gmail.com>
2020-08-10 15:36:55 +0700

    code api-cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userServices = require("../services/cartServices");
 const cartServices = require("../services/cartServices");
-// router.get("/", cartServices.getSession);
-// router.get("/setsession", cartServices.setSession);
+router.get("/", cartServices.checkCartEmpty, cartServices.getCartDetail);
+router.post("/addtocart", cartServices.addToCart);
+// router.get("/addtocart", (req, res, next) => {
+//   res.render("index");
+// });
+router.post('/deleteitem',cartServices.checkCartEmpty,cartServices.deleteItem);
 module.exports = router;
